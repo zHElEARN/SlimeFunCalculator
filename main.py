bf2beb5039a44a77ba43330f821b4f936493ce4b
zHElEARN <zhe_learn@liuzeyun.tech>
2020-04-02 19:05:10 +0800

    (feat) show how many groups

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import utils
 import sys
 import json
 import argparse
+import math
 
-parser = argparse.ArgumentParser(
-    prog="SlimeFunCalculator", description="粘液科技计算器")
+parser = argparse.ArgumentParser(prog="SlimeFunCalculator", description="粘液科技计算器")
 
 parser.add_argument("--items", type=str, help="要查询的所有物品", required=True)
 parser.add_argument("--count", type=str, help="要查询的物品数量", required=True)
@@ -17,7 +17,7 @@ count = args.count.replace(" ", "").split(",")
 
 needed = {}
 
-if (len(items) != len(count)):
+if len(items) != len(count):
     print("Command line parameter error")
     exit(-1)
 
@@ -32,4 +32,16 @@ while utils.calculate(table, needed)[1]:
 
 print("Raw data: {}".format(str(needed)))
 for (key, value) in needed.items():
-    print("{}: {}个".format(key, value))
+    if value > 64:
+        if value != (math.floor(value / 64) * 64):
+            print(
+                "{}: {}组{}个".format(
+                    key,
+                    math.floor(value / 64),
+                    int(value - (math.floor(value / 64) * 64)),
+                )
+            )
+        else:
+            print("{}: {}组".format(key, math.floor(value / 64)))
+    else:
+        print("{}: {}个".format(key, math.ceil(value)))
