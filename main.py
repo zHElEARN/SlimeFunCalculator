38449822c76e5d1a79b3c88d9a53cedb38a4bfb2
zHElEARN <zhe_learn@liuzeyun.tech>
2020-04-02 15:53:25 +0800

    (feat) parsing command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import data_manage
 import utils
+import sys
+import json
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="SlimeFunCalculator", description="粘液科技计算器")
+
+parser.add_argument("--items", type=str, help="要查询的所有物品", required=True)
+parser.add_argument("--count", type=str, help="要查询的物品数量", required=True)
+
+args = parser.parse_args()
+
+items = args.items.replace(" ", "").split(",")
+count = args.count.replace(" ", "").split(",")
+
+needed = {}
+
+if (len(items) != len(count)):
+    print("Command line parameter error")
+    exit(-1)
+
+for i in range(0, len(items)):
+    needed[items[i]] = int(count[i])
 
 table = data_manage.get_table()
-
-
-needed = {'GPS发射器': 10}
 
 while utils.calculate(table, needed)[1]:
     needed = utils.calculate(table, needed)[0]
